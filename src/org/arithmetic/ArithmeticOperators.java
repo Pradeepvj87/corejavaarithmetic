4c1fc5fa037b7461636b06cc7b3905b519d98320
Pradeepvj87 <pradeepvj87@gmail.com>
2020-07-19 08:34:19 +0530

    i am done

---
+++ src/org/arithmetic/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+package org.arithmetic;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
+
+public class ArithmeticOperators {
+public static void main(String[] args) {
+List<Integer> li = new ArrayList<Integer>();	
+	li.add(20);
+	li.add(580);
+	li.add(80);
+	li.add(56);
+	li.add(506);
+for (Integer x : li) {
+	System.out.println(x);
+}
+}
+}
